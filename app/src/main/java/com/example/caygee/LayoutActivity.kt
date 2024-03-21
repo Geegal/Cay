44acb120162f06c6c896ba46f44d2d7d7840d740
ketuyio <asherray9598@gmail.com>
2024-03-21 12:06:23 +0300

    first commit

---
+++ app/src/main/java/com/example/caygee/LayoutActivity.kt
@@ -0,0 +1,160 @@
+package com.example.caygee
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.dp
+import com.example.caygee.ui.theme.CayGeeTheme
+import com.example.caygee.R
+
+class LayoutActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Layout()
+
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Layout(){
+    Column (modifier = Modifier.fillMaxSize()){
+        //TopAppBar
+        TopAppBar(
+            title = { Text(text = "Home", color = Color.White) },
+            colors = TopAppBarDefaults.mediumTopAppBarColors(Color.Red),
+            navigationIcon = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Default.Menu,
+                        contentDescription = "menu",
+                        tint = Color.White)
+
+                }
+            },
+            actions = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Default.Share,
+                        contentDescription = "share",
+                        tint = Color.White)
+
+                }
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Default.Settings,
+                        contentDescription = "settings",
+                        tint = Color.White)
+
+                }
+            },
+
+            )//End of TopAppBar
+        Spacer(modifier = Modifier.height(5.dp))
+        Text(text = "BREEDS OF DOGS",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
+        //Row1
+        Row {
+
+            Image(
+                painter = painterResource(id = R.drawable.syberianhusky),
+                contentDescription ="siberian husky",
+                modifier = Modifier.size(width = 200.dp, height = 150.dp)
+            )
+            Spacer(modifier = Modifier.width(1.dp))
+
+            Column {
+                Text(text = "Siberian Husky", fontWeight = FontWeight.Bold )
+                Text(text = "The Siberian Husky is a medium-sized working sled dog breed. The breed belongs to the Spitz genetic family.")
+            }
+        }
+        //End of Row
+        Spacer(modifier = Modifier.height(20.dp))
+        Row {
+
+            Image(
+                painter = painterResource(id = R.drawable.germanshepherd),
+                contentDescription ="German Shepherd",
+                modifier = Modifier.size(width = 200.dp, height = 150.dp)
+            )
+            Spacer(modifier = Modifier.width(1.dp))
+
+            Column {
+                Text(text = "German Shepherd", fontWeight = FontWeight.Bold )
+                Text(text = "The Siberian Husky is a medium-sized working sled dog breed. The breed belongs to the Spitz genetic family.")
+            }
+        }
+        //End of Row
+        Spacer(modifier = Modifier.height(20.dp))
+        Row {
+
+            Image(
+                painter = painterResource(id = R.drawable.goldenretriever),
+                contentDescription ="Golden retriever",
+                modifier = Modifier.size(width = 200.dp, height = 150.dp)
+            )
+            Spacer(modifier = Modifier.width(1.dp))
+
+            Column {
+                Text(text = "Golden Retriever", fontWeight = FontWeight.Bold )
+                Text(text = "The Siberian Husky is a medium-sized working sled dog breed. The breed belongs to the Spitz genetic family.")
+            }
+        }
+        //End of Row
+        Spacer(modifier = Modifier.height(30.dp))
+        Button(onClick = { /*TODO*/ },
+            shape = RoundedCornerShape(5.dp),
+            colors = ButtonDefaults.buttonColors(Color.Cyan),
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Text(text = "Next")
+        }
+
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LayoutPreview(){
+    Layout()
+
+}
+
---
+++ app/src/main/java/com/example/caygee/MainActivity.kt
@@ -0,0 +1,169 @@
+package com.example.caygee
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.caygee.ui.theme.CayGeeTheme
+import com.example.caygee.LayoutActivity
+import com.example.caygee.R
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Demo()
+
+        }
+    }
+}
+
+@Composable
+fun Demo(){
+    Column(modifier = Modifier.fillMaxSize()) {
+
+        val mContext = LocalContext.current
+
+
+
+        Text(
+            text = "Welcome to Android",
+            color = Color.Red,
+            fontSize = 30.sp,
+            fontStyle = FontStyle.Italic,
+            fontWeight = FontWeight.Bold
+        )
+        Text(text = "Android is an open source and Linux-based operating system for mobile devices such as smartphones and tablet computers. ")
+
+        Spacer(modifier = Modifier.height(15.dp))
+        Column(
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+
+        ) {
+            Button(onClick = {})
+            { Text("see more") }
+        }
+        Text(text = "Types of cars",
+            fontSize = 20.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Red)
+            ,
+            textAlign = TextAlign.Center,
+            textDecoration = TextDecoration.Underline,
+            color = Color.White,
+            fontWeight = FontWeight.Bold)
+        Text(text = "1. BMW")
+        Text(text = "2. Mercedes Benz")
+        Text(text = "3. Ferari")
+        Text(text = "4. Bughatti")
+
+        Spacer(modifier = Modifier.height(15.dp))
+        Text(text = "Courses offered",
+            fontSize = 20.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Red)
+            ,
+            textAlign = TextAlign.Center,
+            textDecoration = TextDecoration.Underline,
+            color = Color.White,
+            fontWeight = FontWeight.Bold)
+        Text(text = "1. MIT")
+        Text(text = "2. CYBER SECURITY")
+        Text(text = "3. DATASCIENCE")
+        Spacer(modifier = Modifier.height(15.dp))
+        Text(text = "Introduction",
+            fontSize = 20.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Red)
+            ,
+
+            textAlign = TextAlign.Center,
+            textDecoration = TextDecoration.Underline,
+            color = Color.DarkGray,
+            fontWeight = FontWeight.Bold
+        )
+        Text(text = "There are various types of cars, each designed for different purposes.")
+
+        Spacer(modifier = Modifier.height(15.dp))
+        Divider()
+
+
+        //CENTERING AN IMAGE start
+        Box (modifier = Modifier.fillMaxWidth(),
+            contentAlignment = Alignment.Center){
+
+            Image(painter = painterResource(id = R.drawable.doggie) ,
+                contentDescription ="C'est un chien.Il est tres beau.Je l' aime.",
+                modifier = Modifier
+                    .size(200.dp)
+                    .clip(shape = CircleShape),
+                contentScale = ContentScale.Crop
+            )
+
+
+        }
+        //CENTERING AN IMAGE end
+        Text(
+            text = "eMobilis Mobile Training Institute",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+        )
+        Spacer(modifier = Modifier.height(15.dp))
+
+        Button(onClick = { mContext.startActivity(Intent(mContext, LayoutActivity::class.java)) },
+            shape = RoundedCornerShape(5.dp),
+            colors = ButtonDefaults.buttonColors(Color.Red),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 30.dp, end = 30.dp)) {
+            Text(text = "Continue")
+
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DemoPreview(){
+    Demo()
+
+}
